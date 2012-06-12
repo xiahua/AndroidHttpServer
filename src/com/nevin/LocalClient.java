d7fdc54d67a2eb35e450c0ed43761cf3b4fdfc30
Nevin <niexinxin1986@gmail.com>
2012-06-12 17:30:04 +0800

    the http server is ok, the downloader is running ok.

---
+++ src/com/nevin/LocalClient.java
@@ -0,0 +1,149 @@
+package com.nevin;
+
+import android.webkit.WebChromeClient;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+import com.nevin.NanoHTTPD;
+import com.nevin.DownloadFileAsync.OnDownloadFinishedListener;
+import com.nevin.DownloadFileAsync.OnDownloadStartedListener;
+import com.nevin.DownloadFileAsync.OnProgressUpdateListener;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.os.Environment;
+import android.widget.TextView;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.util.Log;
+import android.webkit.WebViewClient;
+
+public class LocalClient extends Activity {
+	private final static String TAG = "Local";
+	
+	private ProgressDialog mProgressDialog;
+	public static final int DIALOG_DOWNLOAD_PROGRESS = 0;
+
+	WebView webview;
+	NanoHTTPD httpServer;
+	TextView tv;
+
+	Button butClose;
+
+	/* To ensure we don't open a new window each click. */
+	public class LocalWebViewClient extends WebViewClient {
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			char last = '\0';
+			if( url != null && url.length() > 0 ) last = url.charAt( url.length() - 1 ) ;
+			if (!(last == '/')) {
+				Log.e(TAG,"url: "+url);
+				String decodeUrl = decodeUri(url);
+				String fileName = decodeUrl.substring(decodeUrl.lastIndexOf("/")+1);
+				startDownload(fileName,url);
+				return(true);
+			}
+			else {
+				view.loadUrl(url);
+				return(false);
+			}
+		}
+	}
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		webview = (WebView) findViewById(R.id.webview);
+		webview.setVisibility(android.view.View.INVISIBLE);
+
+		tv = (TextView) this.findViewById(R.id.textview);
+		tv.setText("Ready!");
+	}
+	
+	
+	@Override
+	protected void onStart(){
+		super.onStart();
+		try {
+			httpServer = new NanoHTTPD(8080,Environment.getExternalStorageDirectory());
+			webview = (WebView) findViewById(R.id.webview);
+            webview.setWebViewClient(new LocalWebViewClient());
+            
+            webview.getSettings().setJavaScriptEnabled(true);
+            webview.loadUrl("http://localhost:8080/");
+            webview.setVisibility(android.view.View.VISIBLE);
+		}
+		catch(Exception e) {
+			tv.setText(e.toString());
+		}
+	}
+	
+	@Override
+	protected void onStop(){
+		super.onStop();
+		httpServer.stop();
+	}
+
+	
+	private void startDownload(final String fileName, final String downloadUrl){
+		Log.e(TAG,"startDownload...: "+fileName);
+		Log.e(TAG,"startDownload...: "+downloadUrl);
+		
+		DownloadFileAsync downloader = new DownloadFileAsync();
+		downloader.setOnDownloadStartedListener(new OnDownloadStartedListener() {
+			@Override
+			public void onDownloadStarted() {
+				showDialog(DIALOG_DOWNLOAD_PROGRESS);
+				mProgressDialog.setMessage("downloading "+fileName);
+			}
+		});
+		downloader.setOnProgressUpdateListener(new OnProgressUpdateListener() {
+			@Override
+			public void onProgressUpdate(int progress) {
+				mProgressDialog.setProgress(progress);
+			}
+		});
+		downloader.setOnDownloadFinishedListener(new OnDownloadFinishedListener() {
+			@Override
+			public void onDownloadFinished(String fileName, String downloadUrl) {
+				dismissDialog(DIALOG_DOWNLOAD_PROGRESS);
+			}
+		});
+		
+		downloader.execute(fileName,downloadUrl);
+	}
+	
+	//our progress bar settings
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DIALOG_DOWNLOAD_PROGRESS: //we set this to 0
+                mProgressDialog = new ProgressDialog(this);
+                mProgressDialog.setMessage("Downloading file...");
+                mProgressDialog.setIndeterminate(false);
+                mProgressDialog.setMax(100);
+                mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                mProgressDialog.setCancelable(true);
+                mProgressDialog.show();
+                return mProgressDialog;
+            default:
+                return null;
+        }
+    }
+    
+	private String decodeUri(String uri){
+		String newUri="";
+		try{
+			newUri = URLDecoder.decode(uri, "utf-8");	
+		}catch(UnsupportedEncodingException e){
+			e.printStackTrace();
+		}
+		return newUri;
+	}
+}
